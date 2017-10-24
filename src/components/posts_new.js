577d862ac00f1a0b42554a56f4ce34a3ba42a4b7
sinchi <ayoub.belghar@gmail.com>
2017-10-24 23:35:00 +0100

    add validation form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,28 +3,61 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 
-  renderTitleField(field){
+  renderField(field){
     return (
-      <div>
+      <div className="form-group">
+        <label>{ field.label }</label>
         <input
+          className="form-control"
           type="text"
-          { ...field.input } />
+          { ...field.input }
+        />
+        { field.meta.error }
       </div>
     );
   }
+
 
   render(){
     return (
       <form>
         <Field
+          label="Title For Post"
           name="title"
-          component={ this.renderTitleField }
+          component={ this.renderField }
         />
+        <Field
+          label="Categories"
+          name="categories"
+          component={ this.renderField }
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={ this.renderField }
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     )
   }
 }
 
+function validate(values){
+  const errors = {};
+
+  if(!values.title){
+    errors.title = "Enter a title!";
+  }
+  if(!values.categories){
+    errors.categories = "Enter some categories";
+  }
+  if(!values.content){
+    errors.content = 'Enter some content please';
+  }
+  return errors;
+}
+
 export default reduxForm({
-  form: 'PostsNewForm'
+  validate,
+  form: 'PostsNewForm',
 })(PostsNew);
