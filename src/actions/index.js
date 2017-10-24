e8d562e57e35f43c5dac93671d6802b4eb706f40
sinchi <ayoub.belghar@gmail.com>
2017-10-24 21:41:26 +0100

    mapStateToProps(state){ return { posts: state.posts } }

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,24 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = "FETCH_POSTS";
+export const SHOW_POST = "SHOW_POST";
+
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = '?key=ayoubsinchi';
 
 export function fetchPosts(){
 
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-  
+
   return {
     type: "FETCH_POSTS",
     payload: request
   };
 }
+
+export function showPost(id){
+  return {
+    type: SHOW_POST,
+    payload: {}
+  };
+};
