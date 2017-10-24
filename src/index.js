5ba9815c7da6859a5e59a5e6a7f158dde02b83b4
sinchi <ayoub.belghar@gmail.com>
2017-10-24 22:40:13 +0100

    add redux-form

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 
 import PostsIndex  from './components/posts_index';
+import PostsNew from './components/posts_new';
 
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -15,7 +16,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
         <div>
-          <Route path="/posts" component={PostsIndex} />
+          <Switch>
+            <Route path="/posts/new" component={PostsNew} />
+            <Route path="/" component={PostsIndex} />
+          </Switch>
         </div>
     </BrowserRouter>
   </Provider>
