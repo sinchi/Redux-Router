d4c9c89c535d8b670b572a3fc863bb2982ac37a3
sinchi <ayoub.belghar@gmail.com>
2017-10-25 00:12:42 +0100

    add submittion form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 
   renderField(field){
+    const { meta: { touched, error, pristine } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : '' }`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{ field.label }</label>
         <input
           className="form-control"
           type="text"
           { ...field.input }
         />
-        { field.meta.error }
+        <div className="text-help">
+          { touched && pristine ? error : '' }
+        </div>
       </div>
     );
   }
 
+  onSubmit(values){
+    console.log(values);
+
+  }
 
   render(){
+    const { handleSubmit } = this.props;
     return (
-      <form>
+      <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
         <Field
           label="Title For Post"
           name="title"
@@ -37,6 +47,7 @@ class PostsNew extends Component {
           component={ this.renderField }
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
