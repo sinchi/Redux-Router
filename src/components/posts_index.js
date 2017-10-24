e32995bd5ba528065e2309f63210f13a5c174e1e
sinchi <ayoub.belghar@gmail.com>
2017-10-24 20:56:25 +0100

    install axios and redux-promise

---
+++ src/components/posts_index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class PostsIndex extends Component {
+
+  componentDidMount(){
+    console.log("This is after mounting componentDidMount");
+  }
+
+  render(){
+    return (
+      <div>
+        PostsIndex
+      </div>
+    )
+  }
+}
+
+
+export default PostsIndex;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
 import reducers from './reducers';
+
+import PostsIndex  from './components/posts_index';
+
+import { BrowserRouter, Route } from "react-router-dom";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+        <div>
+          <Route path="/posts" component={PostsIndex} />          
+        </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
